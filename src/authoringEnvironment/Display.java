c4c01f21e01f7ba5a72ddc94c2c5192304057bb9
ar293in2015 <ar293@duke.edu>
2015-03-28 19:24:55 -0400

    added code to levelEdit

---
+++ src/authoringEnvironment/Display.java
@@ -0,0 +1,21 @@
+package authoringEnvironment;
+
+import java.util.Collection;
+
+/**
+ * 
+ * @author anika
+ *
+ */
+public class Display {
+
+	private Collection<Display> myDisplays;
+	
+	private double myWidth, myHeight;
+	
+	public Display(double width, double height)
+	{
+		
+	}
+	
+}
