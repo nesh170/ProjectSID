027175614d0c236ace772a9e317a34371de048a9
MJMendelsohn <mjm111@duke.edu>
2015-03-28 19:37:24 -0400

    Created AuthoringEnvironmentScene

---
+++ src/authoringEnvironment/AuthoringEnvironmentScene.java
@@ -0,0 +1,20 @@
+package authoringEnvironment;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * The scene that contains the entire visual representation of the Game Authoring Environment.
+ * Includes a menu bar and tabs of different editing screens
+ *  
+ * @author Michael
+ *
+ */
+public class AuthoringEnvironmentScene extends Scene {
+
+	public AuthoringEnvironmentScene(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
