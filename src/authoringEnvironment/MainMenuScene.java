85b34a8ee466652396271384b76b3e4eb2552826
MJMendelsohn <mjm111@duke.edu>
2015-03-28 19:42:32 -0400

    Created MainMenuScene

---
+++ src/authoringEnvironment/MainMenuScene.java
@@ -0,0 +1,22 @@
+package authoringEnvironment;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * The scene that contains pops up when the Authoring Environment 
+ * application is opened.
+ * Allows user to select whether they want to create a new game
+ * or edit an existing one.
+ * 
+ * @author Michael
+ *
+ */
+public class MainMenuScene extends Scene {
+
+	public MainMenuScene(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
