e44bcee33813749e10f8f6250310a430d435c13f
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-30 15:22:51 -0400

    implements parts of MediaManager, TODO the rest of it

--- src/media/MediaManager.java
+++ src/media/MediaManager.java
@@ -1,25 +1,106 @@
 package media;
 
-public interface MediaManager {
+import java.net.URL;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+/**
+ * 
+ * @author Ruslan
+ * @author (fill in if you made this stub please)
+ *
+ */
+
+public class MediaManager {
+	
+	// Class variables
+	private static MediaManager sharedInstance;
+	
+	
+	// Instance variables
+	private MediaPlayer mediaPlayer;
+	
+	private URL playURL;
+	private URL pauseURL;
+
+	
+	// Class methods
+	public static MediaManager sharedInstance() {
+		
+		if (sharedInstance == null)
+			sharedInstance = new MediaManager();
+		
+		return sharedInstance;
+		
+	}
+	
+	// Constructor & Helpers
+
+	
+	// All other instance methods
 	/**
-	 * Loads a new media file to play.
+	 * 
+	 * Loads a new media file to play. After this call MediaManager.sharedInstance().play()
+	 * @param String fileLocation
+	 * 
 	 */
-	public void loadNewMedia();
+	public void loadNewMedia(String fileLocation) {
+		this.playURL = getClass().getResource(fileLocation);
+		System.out.println(playURL);
+	}
 	
 	/**
 	 * Pauses the playing of the media content.
 	 */
-	public void pause();
+	public void pause() {
+		
+		if (pauseURL != null) {
+			
+			mediaPlayer.pause();
+			
+			playURL = pauseURL;
+			pauseURL = null;
+			
+		}
+		
+	}
 	
 	/**
-	 * Resumes the playing of some media content.
+	 * Resumes the playing of some media content. Call loadNewMedia(String fileLocation) prior to playing
+	 * 
 	 */
-	public void play();
+	public void play() {
+		
+		// playURL keeps track of next URL to play. don't play the same thing twice by design
+		if (playURL != null) {
+
+			final Media media = new Media(playURL.toString());
+			
+			this.mediaPlayer = new MediaPlayer(media);
+			mediaPlayer.play();
+			
+			pauseURL = playURL;
+			playURL = null;
+			
+		}
+		
+	}
 	
 	/**
 	 * Stops the playing of music and restarts the track.
 	 */
-	public void stop();
+	public void stop() {
+
+		if (pauseURL != null) {
+
+			mediaPlayer.stop();
+
+			playURL = null;
+			pauseURL = null;
+
+		}
+
+	}
 	
 }
