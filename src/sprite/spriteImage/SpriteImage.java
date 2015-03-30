9eb32a82ea24ad0ab4767e47fceae6f47ecbf821
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-29 22:47:03 -0400

    progress on SpriteImage

--- src/sprite/spriteImage/SpriteImage.java
+++ src/sprite/spriteImage/SpriteImage.java
@@ -1,5 +1,70 @@
 package sprite.spriteImage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* 
+* @author Ruslan
+* 
+* Images are stored like so:
+* images.length = # rows = height
+* images[0].length = # columns = width
+* 
+* Please adhere to only setting widths to **odd** values -- centering Sprites will be much easier
+* 
+* A (3 X 2) image would be stored as:
+* [0,0] [1,0] 
+* [0,1] [1,1] 
+* [0,2] [1,2] 
+* 
+*/
+
 public class SpriteImage {
 
+	// Static variables
+	
+	
+	// Instance Variables
+	List<int[][]> images;
+
+	
+	// Getters & Setters
+	public int width() {
+		
+		if (images.get(0)  == null) {
+			return 0;
+		} else {
+
+			int[][] firstImage = images.get(0);
+			int[] firstRow = firstImage[0];
+			return firstRow.length;					// # columns = width, assumes evenly laid-out array (which it should be)
+			
+		}
+		
+	}
+
+	public int height() {
+		
+		if (images.get(0)  == null) {
+			return 0;
+		} else {
+
+			int[][] firstImage = images.get(0);
+			return firstImage.length;				// # rows = height
+			
+		}
+		
+	}
+	
+	// Constructor & Helpers
+	public SpriteImage() {
+		
+		images = new ArrayList<int[][]>();
+		
+	}
+	
+	// All other instance methods
+	
+	
 }
