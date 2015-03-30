3ea03b9ce3fd2e17ce6c0a1a5cd6237dedfbb772
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-30 12:52:59 -0400

    tab height in DOUBLE constants

--- src/resources/constants/DOUBLE.java
+++ src/resources/constants/DOUBLE.java
@@ -5,4 +5,7 @@ public class DOUBLE {
 	public static final double percentHeightMenuBar = 0.03;
 	public static final double percentHeightDisplay = 1 - DOUBLE.percentHeightMenuBar;
 	
+	// ScreenController Tabs
+	public static final double TAB_HEIGHT = 30.0;
+	
 }
