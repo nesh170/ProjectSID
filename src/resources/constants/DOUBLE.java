0e3651ebb86ed3500382aca15fcec76dd2b10482
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-30 11:39:55 -0400

    new DOUBLE constant

---
+++ src/resources/constants/DOUBLE.java
@@ -0,0 +1,8 @@
+package resources.constants;
+
+public class DOUBLE {
+
+	public static final double percentHeightMenuBar = 0.03;
+	public static final double percentHeightDisplay = 1 - DOUBLE.percentHeightMenuBar;
+	
+}
