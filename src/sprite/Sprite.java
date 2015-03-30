55b40bca7645dd41bdf7432048072abf8149bf9e
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-29 21:34:47 -0400

    smarter Sprite constructors

--- src/sprite/Sprite.java
+++ src/sprite/Sprite.java
@@ -51,22 +51,15 @@ public class Sprite {
 	
 	// Constructor & Helpers
 	public Sprite() {
-		
-		setActive(true);
-		myTransform = new Transform(DEFAULT_POSITION, DEFAULT_ROTATION, DEFAULT_DIMENSIONS);
-		
+		this(DEFAULT_POSITION, DEFAULT_ROTATION, DEFAULT_DIMENSIONS);
 	}
 	
 	public Sprite(Point2D coordinate) {
-		
-		setActive(true);
-		myTransform = new Transform(coordinate, DEFAULT_ROTATION, DEFAULT_DIMENSIONS);
-		
+		this(coordinate, DEFAULT_ROTATION, DEFAULT_DIMENSIONS);
 	}
 	
 	public Sprite(Point2D coordinate, Point2D rotate) {
-		setActive(true);
-		myTransform = new Transform(coordinate, rotate, DEFAULT_DIMENSIONS);
+		this(coordinate, rotate, DEFAULT_DIMENSIONS);
 	}
 	
 	public Sprite (Point2D coordinate, Point2D rotate, Dimension2D dimension){
