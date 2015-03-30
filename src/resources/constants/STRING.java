4c1472e867ad610be615c4d05f0b618f54341aef
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-30 14:45:57 -0400

    STRING constants file

---
+++ src/resources/constants/STRING.java
@@ -0,0 +1,8 @@
+package resources.constants;
+
+public class STRING {
+
+	public static final String DEFAULT_FX_BACKGROUND_COLOR = "CORNFLOWERBLUE;";
+	public static final String FX_BACKGROUND_COLOR_PREDICATE = "-fx-background-color: ";
+	
+}
