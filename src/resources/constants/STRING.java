285f206ebb319878eaaa48c18fd75020efafd49e
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-30 18:47:56 -0400

    adds music String constants

--- src/resources/constants/STRING.java
+++ src/resources/constants/STRING.java
@@ -5,4 +5,10 @@ public class STRING {
 	public static final String DEFAULT_FX_BACKGROUND_COLOR = "CORNFLOWERBLUE;";
 	public static final String FX_BACKGROUND_COLOR_PREDICATE = "-fx-background-color: ";
 	
+	// Music Playback
+	public static final String MUSIC_OPTIONS = "Music Options";
+	public static final String PLAY = "Play";
+	public static final String PAUSE = "Pause";
+	public static final String STOP = "Stop";
+	
 }
