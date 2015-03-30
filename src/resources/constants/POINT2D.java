0a9a25875b0135904bd11d860a93f94284c5e639
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-29 21:45:48 -0400

    adds new constants to resources.constants

---
+++ src/resources/constants/POINT2D.java
@@ -0,0 +1,10 @@
+package resources.constants;
+
+import javafx.geometry.Point2D;
+
+public class POINT2D {
+
+	public static final Point2D DEFAULT_POSITION = new Point2D(0.0, 0.0);
+	public static final Point2D DEFAULT_ROTATION = new Point2D(0.0 ,0.0); 
+	
+}
