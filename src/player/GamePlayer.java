61d9ccfbc16d7b0a223d482766588ec9b25498c0
James Mosca <jcm73@duke.edu>
2015-03-31 17:09:55 -0400

    update GamePlayer

--- src/player/GamePlayer.java
+++ src/player/GamePlayer.java
@@ -4,26 +4,92 @@ import java.util.List;
 
 import javafx.animation.Timeline;
 import javafx.scene.Group;
+import gameEngine.GameEngineAbstract;
+import java.util.List;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GamePlayer implements GamePlayerInterface{
 
-	Timeline myAnimation;	
+	private GameEngineAbstract myEngine;
+	private Scene myScene;
+	private BorderPane myBorderPane;
+	private Timeline myTimeline;
+	private int myFrameRate = 30;
+	private String myGameFilePath;
 	
-	public GamePlayer() {
-		//this is just a placeholder, group not actually created here
-		Group root = new Group();
-		myAnimation = new Timeline();
+	public GamePlayer(Stage stage) {
+		myTimeline = new Timeline();
+		myBorderPane = new BorderPane();
+		MenuBar menuBar = new MenuBar();
+        Menu menuEdit = new Menu("Edit");
+        Menu menuView = new Menu("View");
+        menuBar.getMenus().add(buildFileMenu());
+        menuBar.getMenus().add(menuEdit);
+        menuBar.getMenus().add(menuView);
+        
+        myBorderPane.setTop(menuBar);
+		myScene = new Scene(myBorderPane, 1200, 600);
+        stage.setScene(myScene);
+	}
+	
+	private Menu buildFileMenu() {
+		Menu fileMenu = new Menu("File");
 		
+		MenuItem newGameItem = new MenuItem("New Game");
+		newGameItem.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+		newGameItem.setOnAction(event -> { System.out.println("HI"); });
+		
+		MenuItem loadItem = new MenuItem("Load Level");
+		loadItem.setAccelerator(KeyCombination.keyCombination("Ctrl+L"));
+		loadItem.setOnAction(event -> { System.out.println("LOAD"); });
+		
+		MenuItem quitItem = new MenuItem("Quit");
+		quitItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Q"));
+		quitItem.setOnAction(event -> { System.exit(0); });
+		
+		fileMenu.getItems().add(newGameItem);
+		fileMenu.getItems().add(loadItem);
+		fileMenu.getItems().add(quitItem);
+		
+		return fileMenu;
+	}
+	
+	private void initialize(GameEngineAbstract engine) {
+		myEngine = engine;
+		myTimeline = new Timeline();
+		myTimeline.setCycleCount(Animation.INDEFINITE);
+		//myTimeline.getKeyFrames().add(frame);
+	}
+	
+	private void setupAnimation() {
+		//new KeyFrame(Duration.millis(1000 / myFrameRate), e -> updateGrid());
+	}
+	
+	private void update() {
+		Group group = myEngine.render();
 	}
 	
 	@Override
 	public void start() {
-		myAnimation.play();
+		myTimeline.play();
 	}
 
 	@Override
 	public void pause() {
-		myAnimation.stop();
+		myTimeline.stop();
 	}
 
 	@Override
