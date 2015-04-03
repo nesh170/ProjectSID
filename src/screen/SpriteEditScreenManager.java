d3cf8751245625b6a58afe1c5dda12eccfc58708
MJMendelsohn <mjm111@duke.edu>
2015-04-02 22:05:23 -0400

    Resolved Merge

--- src/screen/SpriteEditScreenManager.java
+++ src/screen/SpriteEditScreenManager.java
@@ -7,11 +7,6 @@ import sprite.Sprite;
 // Inner class for handling SpriteEditScreenController methods
 public class SpriteEditScreenManager extends UniversalManager implements SpriteEditScreenController {
 	
-//	@Override
-	public void returnToSelectedLevel() {
-		throw new IllegalStateException("unimplemented returnToSelectedLevel in SpriteEditScreenController");
-	}
-
 	@Override
 	public void returnToSelectedLevel(Tab tab, Sprite sprite) {
 		// TODO Auto-generated method stub
