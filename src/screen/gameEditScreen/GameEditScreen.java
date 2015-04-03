c91a06f0b0156734230ea92dcf5971f6f42529b2
ar293in2015 <ar293@duke.edu>
2015-04-02 21:44:42 -0400

    added button constants

--- src/screen/gameEditScreen/GameEditScreen.java
+++ src/screen/gameEditScreen/GameEditScreen.java
@@ -6,6 +6,13 @@ import screen.Screen;
 
 public class GameEditScreen extends Screen {
 
+	private static final int NUM_BUTTONS = 6;
+	private static final int SAVE_BUTTON = 0;
+	private static final int PLAY_BUTTON = 1;
+	private static final int REMOVE_LEVEL_BUTTON = 2;
+	private static final int REMOVE_SPLASH_BUTTON = 3;
+	private static final int ADD_LEVEL_BUTTON = 4;
+	private static final int ADD_SPLASH_BUTTON = 5;
 	public GameEditScreen(double width, double height) {
 		
 		super(width, height);
