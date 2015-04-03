5887bacf13e9e7a17bbf6c8b32459afc0aaea793
ar293in2015 <ar293@duke.edu>
2015-04-02 21:45:02 -0400

    added button style constant

--- src/screen/gameEditScreen/GameEditScreen.java
+++ src/screen/gameEditScreen/GameEditScreen.java
@@ -13,6 +13,10 @@ public class GameEditScreen extends Screen {
 	private static final int REMOVE_SPLASH_BUTTON = 3;
 	private static final int ADD_LEVEL_BUTTON = 4;
 	private static final int ADD_SPLASH_BUTTON = 5;
+	private static final String BUTTON_STYLE = 
+			"-fx-font: 14 georgia; -fx-text-fill: black;  "
+			+ "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 ); "
+			+ "-fx-border-width: 2 2 2 2; -fx-border-color: #006652; -fx-background-color: white;";
 	public GameEditScreen(double width, double height) {
 		
 		super(width, height);
