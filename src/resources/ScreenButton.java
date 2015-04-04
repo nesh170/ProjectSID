c05d59334c6a37397516a958ac1fb7a2f5b4e9f6
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-04 15:15:43 -0400

    uses STRING constants in ScreenButton

--- src/resources/ScreenButton.java
+++ src/resources/ScreenButton.java
@@ -8,25 +8,27 @@ import javafx.scene.control.Button;
  * @author anika
  *
  */
-public class ScreenButton extends Button{
-	public ScreenButton(String name, String buttonStyle){
+public class ScreenButton extends Button {
+	
+	public ScreenButton(String name, String buttonStyle) {
+		
 		this.setText(name);
 		this.setStyle(buttonStyle);
 		this.setOnMousePressed(e -> mouseDown());
 		this.setOnMouseReleased(e -> mouseUp());
+		
 	}
 	
 	/**
 	 * added button styles
 	 * @author Anika
 	 */
-	private void mouseDown()
-	{
-		this.setStyle("-fx-font: 14 georgia; -fx-text-fill: #CC0000;  -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) ,.5, 0.0 , 0 , 1 ); -fx-border-width: 2 2 2 2; -fx-border-color: white; -fx-background-color: black;");
+	private void mouseDown() {
+		this.setStyle(STRING.PRESSED_BUTTON_CSS);
 	}
 	
-	private void mouseUp()
-	{
-		this.setStyle("-fx-font: 14 georgia; -fx-text-fill: black;  -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 ); -fx-border-width: 2 2 2 2; -fx-border-color: #006652; -fx-background-color: white;");
+	private void mouseUp() {
+		this.setStyle(STRING.RELEASED_BUTTON_CSS);
 	}
+	
 }
