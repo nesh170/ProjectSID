8040207ba36d9bd01fdce2e7721ac6a1ee27ecd9
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-04 14:36:33 -0400

    formatting nits in LevelView

--- src/levelPlatform/level/LevelView.java
+++ src/levelPlatform/level/LevelView.java
@@ -39,38 +39,49 @@ public class LevelView {
      * @return
      */
     public Group renderLevel() {
+    	
         Group levelGroup = new Group();
         myLevel.sprites().stream().forEach(sprite -> levelGroup.getChildren().add(renderSprite(sprite)));
         return levelGroup;
+        
     }
     
     private Group renderSprite(Sprite sprite) {
+    	
     	Group spriteGroup = new Group();
     	Transform transform = sprite.transform();
         Rectangle spriteNode = new Rectangle(transform.getPosX(),transform.getPosY(),transform.getWidth(),transform.getHeight());
         Paint spriteColor;
+        
         try {
             spriteColor = Color.web(sprite.path());
         } 
+        
         catch(IllegalArgumentException e) {
+        	
             //TODO add Ruslan's sprite image methodology
             //spriteColor = new ImagePattern(resourceManager.getImage(myColorPath));
             spriteColor = Color.BEIGE;
             
         }
+        
         spriteNode.setFill(spriteColor);
         spriteGroup.getChildren().add(spriteNode);
         sprite.emissionList().stream().forEach(emission -> spriteGroup.getChildren().add(renderSprite(emission)));
+        
         return spriteGroup;
+        
     }
     
     /**
      * Checks for collision with each node
      */
     public void updateCollisions(){
-        for(Sprite sprite:myLevel.sprites){
+    	
+        for(Sprite sprite:myLevel.sprites()){
         	myLevel.sprites().stream().forEach(sprite2 -> handleCollisions(sprite,sprite2));
         }
+        
     }
     
     /**
@@ -80,12 +91,21 @@ public class LevelView {
      * @param sprite2
      */
     private void handleCollisions(Sprite sprite1, Sprite sprite2){
+    	
         if(sprite1.equals(sprite2)){
             return;
         }
-        if(renderSprite(sprite1).getChildren().get(0).getBoundsInParent().intersects((renderSprite(sprite2).getChildren().get(0).getBoundsInParent()))){
+        
+        Bounds boundsSprite1 = renderSprite(sprite1).getChildren().get(0).getBoundsInParent();
+        Bounds boundsSprite2 = (renderSprite(sprite2).getChildren().get(0).getBoundsInParent());
+        
+        if(boundsSprite1.intersects(boundsSprite2)) {
+        	
             myCollisionHandler.handleCollide(sprite1, sprite2);
             //TODO should we just act on sprite 1 or both sprite1 and sprite 2
+            
         }
+        
     }
+    
 }
