ceeb3dcef3245993b52e31152618009a6ec15987
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-05 14:20:28 -0400

    adds a default length side pixel to DOUBLE for LevelView

--- src/resources/constants/DOUBLE.java
+++ src/resources/constants/DOUBLE.java
@@ -5,10 +5,12 @@ public class DOUBLE {
 	// Percentages
 	public static final double NINETY_PERCENT = 0.90;
 
+	
 	// Screen
 	public static final double percentHeightMenuBar = 0.03;
 	public static final double percentHeightDisplay = 1 - DOUBLE.percentHeightMenuBar;
 	public static final double BUTTON_SPACING = 20;
+	
 	
 	// ScreenController
 	// Tabs
@@ -17,7 +19,12 @@ public class DOUBLE {
 	public static final double ERROR_MESSAGE_WIDTH = 200.0;
 	public static final double ERROR_MESSAGE_HEIGHT = 80.0;
 	
+	
 	//Default move speed for some non-player sprites
 	public static final double DEFAULT_TOWARD_MULTIPLIER = 0.1;
+	
+	
+	// LevelView
+	public static final double DEFAULT_LENGTH_SIDE_PIXEL = 5.0;
 	
 }
