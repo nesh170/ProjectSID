9e1b601f0bcd1c4898a6fd252c0c452b9339f0aa
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-06 03:00:36 -0400

    renames ScreenControllerInterface to ScreenDisplayingInterface

---
+++ src/screen/ScreenDisplayingInterface.java
@@ -0,0 +1,34 @@
+package screen;
+
+import screen.manager.Manager;
+import javafx.scene.ImageCursor;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author Ruslan
+ * 
+ */
+
+public interface ScreenDisplayingInterface {
+
+	// Overall width, height
+	public double width();
+	public double height();
+	
+	// For instantiating new Screen instances
+	public double newScreenWidth();
+	public double newScreenHeight();
+	public double addTabWithScreenWithControllerWithIndentifier(Screen screen, Manager manager, String identifier);
+	
+	// JavaFX
+	public Stage stage();
+	public Scene scene();
+	// Cursor
+	public void setCursor(ImageCursor imageCursor);
+	
+	// Display invalid commands message
+	public void displayError(String error);
+	
+}
