36ac1691caeb572d29baa010977c4cd5ab850fb5
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-06 04:06:55 -0400

    adds a level and parent controller to GamePlayScreen

--- src/screen/gamePlayScreen/GamePlayScreen.java
+++ src/screen/gamePlayScreen/GamePlayScreen.java
@@ -8,13 +8,29 @@ import screen.Screen;
 
 public class GamePlayScreen extends Screen {
 
+	// Instance Variables
+	private GamePlayScreenController parent;
+	private Level level;
 	private GamePlayer myPlayer;
 	
-	public GamePlayScreen(double width, double height) {
+	public GamePlayScreen(GamePlayScreenController parent, double width, double height, Level level) {
+		
 		super(width, height);
+		
+		configureParent(parent);
+		configureLevel(level);
 		//maybe change? adding creating GamePlayer here so screen can get MenuBar
 		//also every GamePlayer must be containted within a gamePlayer screen
 		myPlayer = new GamePlayer(width, height);		
+		
+	}
+	
+	private void configureParent(GamePlayScreenController parent) {
+		this.parent = parent;
+	}
+	
+	private void configureLevel(Level level) {
+		this.level = level;
 	}
 
 	//GamePlayer specific menuBar
