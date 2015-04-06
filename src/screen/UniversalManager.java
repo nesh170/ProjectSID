d26d71afada6646a4815af4dd91a02ab30de74fc
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-06 02:26:47 -0400

    descriptive comments in UniversalManager

--- src/screen/UniversalManager.java
+++ src/screen/UniversalManager.java
@@ -5,6 +5,26 @@ import java.io.File;
 
 public abstract class UniversalManager {
 	
+	// Static Variables
+	
+	
+	// Instance Variables
+	
+	
+	// Getters & Setters
+	
+	
+	
+	// Constructor & Helpers
+	
+	/**
+	 * Yes, this is a setter. It's only supposed to be used once, however.
+	 * @param parent
+	 */
+	
+	
+	// All other instance methods
+	// Public
 	public void handleError(String message) {
 //		Dialog<String> dialog = new Dialog<>();
 //		dialog.setContentText(message);
@@ -17,5 +37,7 @@ public abstract class UniversalManager {
 		throw new IllegalStateException("Unimplemented handleSound method");
 		
 	}
+	
+	// Private
 	
 }
