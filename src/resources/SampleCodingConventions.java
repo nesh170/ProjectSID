5a90c92a9aa0bfb794382fe22f1938cf3b5a97df
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-06 02:26:17 -0400

    fills in some Sample code conventions

--- src/resources/SampleCodingConventions.java
+++ src/resources/SampleCodingConventions.java
@@ -25,7 +25,38 @@ public class SampleCodingConventions {
 
 	// All other instance methods
 	// Public
-
+	public void doNothing() {
+		
+		if (true) {
+			
+			// Do nothing with spacing! Yay!
+			// I'm a two-liner. I get a new line on top and bottom!
+			
+		}
+		
+		else {
+			// I'm a one-liner. I get no spacing.
+		}
+		
+		bobWithBooleanWithString(false, "hullo");
+		
+	}
+	
 	// Private
+	private Object bobWithBooleanWithString(boolean noWay, String yeahWay) {
 
+		Object returnObject = null;
+		
+		if (noWay) {
+			System.out.println(yeahWay);
+		}
+		
+		else {
+			returnObject = new Object();
+		}
+		
+		return returnObject;
+		
+	}
+	
 }
