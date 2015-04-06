5cbbb007ff67244699c3f6b39711626c1dbdcdc5
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-06 02:22:55 -0400

    adds Sample Coding Conventions for readability

---
+++ src/resources/SampleCodingConventions.java
@@ -0,0 +1,31 @@
+package resources;
+
+
+/**
+ * Descriptive description here. NB the spacing between large categories (two lines), minor categories (one line)
+ * 
+ * @author Ruslan
+ */
+public class SampleCodingConventions {
+
+	// Static Variables
+
+
+	// Instance Variables
+
+
+	// Getters & Setters
+
+
+	// Static Methods
+
+
+	// Constructor & Helpers
+
+
+	// All other instance methods
+	// Public
+
+	// Private
+
+}
