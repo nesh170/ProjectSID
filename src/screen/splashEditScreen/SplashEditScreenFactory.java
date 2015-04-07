245cd3f990b7657c10c7dd46bb677fc341492568
Kyle Milden <kam101@duke.edu>
2015-04-07 18:19:52 -0400

    nothing, waiting for refactor

--- src/screen/splashEditScreen/SplashEditScreenFactory.java
+++ src/screen/splashEditScreen/SplashEditScreenFactory.java
@@ -1,5 +1,7 @@
 package screen.splashEditScreen;
 
+import levelPlatform.splashScreen.SplashScreen;
+
 /**
  * Factory used to create SplashEditScreens. Allows for extensibility
  * and abstraction of Screens.
@@ -10,10 +12,15 @@ public class SplashEditScreenFactory {
 	
 	private double width;
 	private double height;
+	private SplashScreen splashScreen;
 	
 	public SplashEditScreenFactory(double width, double height) {
 		this.width = width;
 		this.height = height;
+	}
+
+	public void add() {
+		// TODO Auto-generated method stub
 	}
 	
 //	public SplashEditScreen createGameEditScreen(SplashEditScreenController splashEditScreenController) {
