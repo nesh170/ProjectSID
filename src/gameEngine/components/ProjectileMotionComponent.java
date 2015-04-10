5f2160981894f643855a49801753f525a88c0422
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-04-10 04:06:42 -0400

    added constants for bullet

--- src/gameEngine/components/ProjectileMotionComponent.java
+++ src/gameEngine/components/ProjectileMotionComponent.java
@@ -1,6 +1,7 @@
 package gameEngine.components;
 
 import javafx.geometry.Point2D;
+import resources.constants.DOUBLE;
 import sprite.Sprite;
 import gameEngine.components.VelocityComponent;
 
@@ -19,10 +20,10 @@ public class ProjectileMotionComponent extends VelocityComponent {
 		//override for any possible movement
 		//algorithm here.
 		//TODO: REMOVE HARDCODED SPEED
-		if((mySprite.transform().getPosX() - myShooter.transform().getPosX()) > 1000){
+		if((mySprite.transform().getPosX() - myShooter.transform().getPosX()) > DOUBLE.BULLET_SELF_DESTRUCT_DIST){
 			mySprite.setIsActive(false);
 		}
-		setVelocity(new Point2D(50.0, 0.0));
+		setVelocityX(DOUBLE.BULLET_SPEED);
 
 	}
 
