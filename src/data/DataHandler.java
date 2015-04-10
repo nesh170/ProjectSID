4f603e2e9b59bbe6ac7b58215e5a199f5aa02bbf
Le Qi <le.qi19@gmail.com>
2015-04-09 23:39:33 -0400

    Added Video classes and methods

--- src/data/DataHandler.java
+++ src/data/DataHandler.java
@@ -1,5 +1,7 @@
 package data;
 
+import game.Game;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -43,7 +45,7 @@ public class DataHandler {
 		directoryChooser.setTitle("Open Directory");
 		return directoryChooser.showDialog(stage);
 	}
-	
+
 	public static File chooseFile(Stage stage) {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Select File");
@@ -63,11 +65,11 @@ public class DataHandler {
 						|| file.toString().endsWith(".tif")
 						|| file.toString().endsWith(".tiff")
 						|| file.toString().endsWith(".gif"))
-				.map(file -> fileToImage(file))
-				.collect(Collectors.toList());
+				.map(file -> fileToImage(file)).collect(Collectors.toList());
 	}
-	
-	public static List<Image> getImagesFromDir(File folder, double maxWidth, double maxHeight, boolean preserve) throws IOException {
+
+	public static List<Image> getImagesFromDir(File folder, double maxWidth,
+			double maxHeight, boolean preserve) throws IOException {
 		return Arrays
 				.asList(folder.listFiles())
 				.stream()
@@ -79,15 +81,12 @@ public class DataHandler {
 				.map(file -> fileToImage(file, maxWidth, maxHeight, preserve))
 				.collect(Collectors.toList());
 	}
-	
+
 	public static List<Level> getLevelsFromDir(File folder) throws IOException {
-		return Arrays
-				.asList(folder.listFiles())
-				.stream()
+		return Arrays.asList(folder.listFiles()).stream()
 				.filter(file -> file.toString().endsWith(".xml"))
 				.map(file -> fromXMLFile(file))
-				.map(obj -> Level.class.cast(obj))
-				.collect(Collectors.toList());
+				.map(obj -> Level.class.cast(obj)).collect(Collectors.toList());
 	}
 
 	public static Image URLToImage(String url) {
@@ -99,26 +98,34 @@ public class DataHandler {
 		}
 		return new Image(url);
 	}
-	
+
 	public static Image fileToImage(File file) {
 		return new Image(file.toURI().toString());
 	}
-	
+
 	/**
-	 * Call this function to obtain a JavaFX Image object from an image file. This specific method
-	 * allows you to specify the maximum width and height of the new Image object. If the preserve
-	 * input is true, the file's image ratio is preserved, and kept within the bounds of the box.
-	 * If preserve is false, then the image simply has a width of maxWidth, and a height of
+	 * Call this function to obtain a JavaFX Image object from an image file.
+	 * This specific method allows you to specify the maximum width and height
+	 * of the new Image object. If the preserve input is true, the file's image
+	 * ratio is preserved, and kept within the bounds of the box. If preserve is
+	 * false, then the image simply has a width of maxWidth, and a height of
 	 * maxHeight.
 	 * 
-	 * @param file		The image file that represents the image you want to convert to JavaFX.
-	 * @param maxWidth	The maximum width of the JavaFX image.
-	 * @param maxHeight	The maximum height of the JavaFX image.
-	 * @param preserve	whether the original file's aspect ratio is fixed.
+	 * @param file
+	 *            The image file that represents the image you want to convert
+	 *            to JavaFX.
+	 * @param maxWidth
+	 *            The maximum width of the JavaFX image.
+	 * @param maxHeight
+	 *            The maximum height of the JavaFX image.
+	 * @param preserve
+	 *            whether the original file's aspect ratio is fixed.
 	 * @return a JavaFX Image object.
 	 */
-	public static Image fileToImage(File file, double maxWidth, double maxHeight, boolean preserve) {
-		return new Image(file.toURI().toString(), maxWidth, maxHeight, preserve, true);
+	public static Image fileToImage(File file, double maxWidth,
+			double maxHeight, boolean preserve) {
+		return new Image(file.toURI().toString(), maxWidth, maxHeight,
+				preserve, true);
 	}
 
 }
