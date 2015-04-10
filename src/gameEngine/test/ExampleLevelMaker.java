984c95e7df6c66dc432911c19184f5a1d9fb2066
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-04-10 03:08:16 -0400

    new cool example level from Patrick

--- src/gameEngine/test/ExampleLevelMaker.java
+++ src/gameEngine/test/ExampleLevelMaker.java
@@ -10,7 +10,9 @@ import gameEngine.actions.KillAction;
 import gameEngine.actions.LeftMotionAction;
 import gameEngine.actions.NormalAction;
 import gameEngine.actions.RightMotionAction;
+import gameEngine.actions.ShootAction;
 import gameEngine.components.HealthComponent;
+import gameEngine.components.ProjectileMotionComponent;
 import gameEngine.components.VelocityComponent;
 
 import java.util.ArrayList;
@@ -28,51 +30,93 @@ import levelPlatform.level.Level;
 
 public class ExampleLevelMaker {
 	
-	public static void main(String[] args){
-		System.out.println("Oh yeah!!!");
-		//set up player
-		Sprite player = new Sprite();
-		player.addComponent(new HealthComponent(player));
-		player.addComponent(new VelocityComponent(player));
-		player.addAction(new LeftMotionAction(player, 20.0, KeyCode.LEFT));
-		Action rma = new RightMotionAction(player, 20.0, KeyCode.RIGHT);
-		player.addAction(rma);
-		Action jumpAction = new JumpAction(player, -6.0, KeyCode.UP);
-		player.addAction(jumpAction);
-		Action gravityAction = new FallAction(player, 10.0);
+	private static final double GRAVITY = 50.0;
+	private static final double SPEED = 8.0;
+	private static final double JUMP_SPEED = -20.0;
+	
+	private Sprite myPlayer;
+	private List<Sprite> mySpriteList;
+	private Action myJumpAction;
+	private Action myNormalAction;
+	private CollisionTable myCT;
+	private Action myKillAction;
+	
+	private Level makeLevel(){
+	System.out.println("Oh yeah!!!");
+	//set up player
+	myPlayer = new Sprite(new Point2D(0.0, 100.0), Point2D.ZERO, new Dimension2D(50.0, 50.0));
+	addPlayerComponentsAndActions();
+	mySpriteList = new ArrayList<Sprite>();
+	mySpriteList.add(myPlayer);
+	Level l = new Level(500, 500, myPlayer);
+	//set up collisions
+	myCT = new CollisionTable();
+	myPlayer.setCollisionTag("player");
+	l.setCollisionTable(myCT);
+	//make Platforms here:
+	makePlatform(0, 400, 500, 10, myNormalAction, "platform");
+	makePlatform(600, 350, 300, 10, myJumpAction, "trampoline");
+	makePlatform(1000, 300, 500, 10, myNormalAction, "platform");
+	//Enemy:
+	makeEnemy(660, 160, 30, 30);
+	makeEnemy(780, 160, 30, 30);
+	makeEnemy(200, 300, 30, 100);
+	
+	//set up projectile template, add to player, along with shoot actions
+	Sprite projTemp = new Sprite(new Point2D(0,0), Point2D.ZERO, new Dimension2D(10, 10));
+	ProjectileMotionComponent projComp = new ProjectileMotionComponent(projTemp);
+	projTemp.addComponent(projComp);
+	Action shootAction = new ShootAction(myPlayer, projTemp, KeyCode.SPACE);
+	myPlayer.addAction(shootAction);
+	
+	
+	//Goals:
+	Map<Sprite, Integer> goalMap = new HashMap<>();
+	goalMap.put(myPlayer, 0);
+	
+	l.setSprites(mySpriteList);
+	l.setGoalMap(goalMap);
+	return l;
+}
+	private void makeEnemy(double x, double y, double width, double height) {
+		Sprite enemy = new Sprite(new Point2D(x, y),Point2D.ZERO,new Dimension2D(width, height));
+		enemy.setCollisionTag("enemy");
+		myCT.addActionToMap(myPlayer.collisonTag(), enemy.collisonTag(), INT.COLLISION_UP, myKillAction);
+		myCT.addActionToMap(enemy.collisonTag(), myPlayer.collisonTag(), INT.COLLISION_DOWN, null);
+		myCT.addActionToMap(myPlayer.collisonTag(), enemy.collisonTag(), INT.COLLISION_DOWN, myKillAction);
+		myCT.addActionToMap(enemy.collisonTag(), myPlayer.collisonTag(), INT.COLLISION_UP, null);
+		myCT.addActionToMap(myPlayer.collisonTag(), enemy.collisonTag(), INT.COLLISION_RIGHT, myKillAction);
+		myCT.addActionToMap(enemy.collisonTag(), myPlayer.collisonTag(), INT.COLLISION_LEFT, null);
+		myCT.addActionToMap(myPlayer.collisonTag(), enemy.collisonTag(), INT.COLLISION_LEFT, myKillAction);
+		myCT.addActionToMap(enemy.collisonTag(), myPlayer.collisonTag(), INT.COLLISION_RIGHT, null);
+		mySpriteList.add(enemy);
+	}
+	private void addPlayerComponentsAndActions() {
+		myPlayer.addComponent(new HealthComponent(myPlayer));
+		myPlayer.addComponent(new VelocityComponent(myPlayer));
+		myPlayer.addAction(new LeftMotionAction(myPlayer, SPEED, KeyCode.LEFT));
+		Action rma = new RightMotionAction(myPlayer, SPEED, KeyCode.RIGHT);
+		myPlayer.addAction(rma);
+		myJumpAction = new JumpAction(myPlayer, JUMP_SPEED, KeyCode.UP);
+		myPlayer.addAction(myJumpAction);
+		Action gravityAction = new FallAction(myPlayer, GRAVITY);
 		gravityAction.runEveryFrame();
-		player.addAction(gravityAction);
-		Action normalAction = new NormalAction(player);
-		player.addAction(normalAction);
-		//set up platform
-
-		Sprite platform = new Sprite(new Point2D(0, 430),Point2D.ZERO,new Dimension2D(500, 10));
-		
-		Action killAction = new KillAction(player, 0.0, KeyCode.UP);
-		
-
-
-		List<Sprite> spriteList = new ArrayList<Sprite>();
-		spriteList.add(platform);
-		spriteList.add(player);
-		spriteList.add(platform);
-		Level l = new Level(5000, 500, player);
-		
-		//set up collisions
-		CollisionTable ct = new CollisionTable();
-		platform.setCollisionTag("platform");
-		player.setCollisionTag("player");
-		ct.addActionToMap(player.collisonTag(), platform.collisonTag(), INT.COLLISION_UP, killAction);
-		ct.addActionToMap(platform.collisonTag(), player.collisonTag(), INT.COLLISION_DOWN, null);
-		l.setCollisionTable(ct);
-
-		
-		Map<Sprite, Integer> goalMap = new HashMap<>();
-		goalMap.put(player, 0);
-		
-
-		l.setSprites(spriteList);
-		l.setGoalMap(goalMap);
+		myPlayer.addAction(gravityAction);
+		myNormalAction = new NormalAction(myPlayer);
+		myPlayer.addAction(myNormalAction);
+		myKillAction = new KillAction(myPlayer, 0.0, KeyCode.K);
+	}
+	private void makePlatform(double x, double y, double width, double height, Action action, String tag) {
+		Sprite platform = new Sprite(new Point2D(x, y),Point2D.ZERO,new Dimension2D(width, height));
+		platform.setCollisionTag(tag);
+		myCT.addActionToMap(myPlayer.collisonTag(), platform.collisonTag(), INT.COLLISION_UP, action);
+		myCT.addActionToMap(platform.collisonTag(), myPlayer.collisonTag(), INT.COLLISION_DOWN, null);
+		mySpriteList.add(platform);
+	}
+	
+	public static void main(String[] args){
+		ExampleLevelMaker elm = new ExampleLevelMaker();
+		Level l = elm.makeLevel();
 		try{
 		DataHandler.toXMLFile(l, "exampleLevel.xml", System.getProperty("user.dir")+"/mario");
 		}
