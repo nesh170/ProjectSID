0be29c60b58c7a0d1e757c22936410e704d2042c
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 08:09:37 -0400

    stub for LevelPlatformView and SIDPixel testing

---
+++ src/tester/levelPlatform/levelPlatformView/LevelPlatformViewTester.java
@@ -0,0 +1,27 @@
+package tester.levelPlatform.levelPlatformView;
+
+import javafx.stage.Stage;
+import tester.Tester;
+
+
+/**
+ * Test displaying a LevelPlatformView with SIDPixels
+ * @author Ruslan
+ *
+ */
+public class LevelPlatformViewTester extends Tester {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	protected void test(Stage stage) {
+
+//		LevelPlatformView levelPlatformView = new LevelPlatformView();
+		
+	}
+
+	
+	
+}
