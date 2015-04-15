819ce0f7b55b4412f65ecc333fdf9415b307a803
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 07:20:04 -0400

    SIDPixel stub

---
+++ src/levelPlatform/levelPlatformView/SIDPixel.java
@@ -0,0 +1,16 @@
+package levelPlatform.levelPlatformView;
+
+import javafx.scene.layout.Region;
+
+
+/**
+ * The perfect JavaFX class, subclassed for the task. Implements Styleable for bounds, EventTarget for mouse interactions
+ * Contained in a LevelPlatformView -> TilePane 
+ * 
+ * @author Ruslan
+ */
+public class SIDPixel extends Region {
+
+	
+	
+}
