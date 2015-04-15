2feb5336f0f3fd9164e8acf7262d94aee0489902
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 17:31:19 -0400

    stub for a LevelPlatformCapableScreen - shared LevelPlatformView (2D grid of SIDPixel)

---
+++ src/screen/levelPlatformCapableScreen/LevelPlatformCapableScreen.java
@@ -0,0 +1,21 @@
+package screen.levelPlatformCapableScreen;
+
+import screen.Screen;
+
+/**
+ * Minimizes duplicated code via inheritance of a LevelPlatformView (2D grid)
+ * in the central viewableArea(). 
+ * 
+ * @author Ruslan
+ */
+public abstract class LevelPlatformCapableScreen extends Screen {
+
+	public LevelPlatformCapableScreen(double width, double height) {
+
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
