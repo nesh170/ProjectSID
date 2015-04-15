780dbcc916a56626ddca6b8e5e3e702254c2dd19
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 13:39:57 -0400

    [NO CHANGE] CMD + i magic

--- src/screen/Screen.java
+++ src/screen/Screen.java
@@ -45,8 +45,8 @@ public abstract class Screen extends BorderPane {
 	// Sizing
 	private double sideBarWidth;
 	private double buttonWidth;
-	
-	
+
+
 	// Getters & Setters
 	/**
 	 * Used in ScreenController addTabWithScreenWithStringIdentifier.
@@ -57,29 +57,28 @@ public abstract class Screen extends BorderPane {
 	public void setParentTab(Tab parentTab) {
 		this.parentTab = parentTab;
 	}
-	
+
 	protected BorderPane viewableArea() {
 		return this.viewableArea;
 	}
-	
+
 	protected ResourceBundle languageResources() {
 		return this.languageResources;
 	}
-	
+
 	protected ResourceBundle tagResources() {
 		return this.tagResources;
 	}
-	
+
 	protected double sideBarWidth() {
 		return this.sideBarWidth;
 	}
-	
+
 	protected double buttonWidth() {
 		return this.buttonWidth;
 	}
 
-	}
-	
+
 	// Constructor & Helpers
 	/**
 	 * 
@@ -88,136 +87,136 @@ public abstract class Screen extends BorderPane {
 	 * 
 	 */
 	public Screen(double width, double height) {
-		
+
 		instantiateViewableArea();
 		configureSizing(width, height);		// depends on an instance of viewableArea existing
 		configureMenuBar(width);
 		configureBackgroundColor();
 		initializeRelevantResourceFiles();
-				
+
 	}
-	
+
 	private void configureSizing(double width, double height) {
-		
+
 		configureWidthAndHeight(width, height);
 		configureSideBarAndButtonSizing(width);
 		configureViewableAreaSizing(width, height);
-		
+
 	}
-	
+
 	private void configureWidthAndHeight(double width, double height) {
-		
+
 		setMinMaxWidthOnNode(this, width);
 		setMinMaxHeightOnNode(this, height);
-		
+
 	}
-	
+
 	private void setMinMaxWidthOnNode(Node node, double width) {
-		
+
 		this.setMinWidth(width);
 		this.setMaxWidth(width);
-		
+
 	}
-	
+
 	private void setMinMaxHeightOnNode(Node node, double height) {
-		
+
 		this.setMinHeight(height);
 		this.setMaxHeight(height);
-		
+
 	}
-	
+
 	private void instantiateViewableArea() {
-		
+
 		this.viewableArea = new BorderPane();
 		this.setCenter(viewableArea);
-		
+
 	}
-	
+
 	/**
 	 * passes MenuBar to abstract subclass method
 	 */
 	private void configureMenuBar(double width) {
-		
+
 		VBox menuBarWrapper = instantiateMenuBar();
 		addMenuItemsToMenuBar(menuBar);				// passes MenuBar to abstract method, never exposes MenuBar instance variable
 		addMenuBarToThis(menuBarWrapper);
-		
+
 	}
-	
+
 	private VBox instantiateMenuBar() {
-		
+
 		VBox menuBarWrapper = new VBox();
-		
+
 		menuBar = new MenuBar();
 		VBox.setVgrow(menuBar, Priority.NEVER);
 		menuBarWrapper.getChildren().add(menuBar);
-		
+
 		menuBar.setPrefWidth(this.getWidth());
 		menuBar.setPrefHeight(DOUBLE.MENU_BAR_HEIGHT);
-		
+
 		return menuBarWrapper;
-		
+
 	}
-	
+
 	protected abstract void addMenuItemsToMenuBar(MenuBar menuBar);
-	
+
 	@SafeVarargs
 	protected final Menu makeFileMenu(EventHandler<ActionEvent>... fileMenuActions) {	
-		
+
 		//TODO more menu items for file
 		MenuItem save = new MenuItem("Save");
 		MenuItem exit = new MenuItem("Exit");
 		MenuItem saveAndExit = new MenuItem("Save and Exit");
-		
+
 		save.setOnAction(fileMenuActions[0]);
 		exit.setOnAction(fileMenuActions[1]);
 		saveAndExit.setOnAction(fileMenuActions[2]);
-		
+
 		Menu fileMenu = new Menu("File");
 		fileMenu.getItems().addAll(save, exit); //removed saveAndExit temporarily since it's not working by Yongjiao 
 
 		return fileMenu;
-		
+
 	}
-	
+
 	protected void initializeRelevantResourceFiles() {
-		
+
 		languageResources = ResourceBundle.getBundle("resources.stringResources");
 		tagResources = ResourceBundle.getBundle("resources.TagChoices");
-		
+
 	}
 
 	private void addMenuBarToThis(VBox wrapper) {
 		this.setTop(wrapper);
 	}
-	
+
 	private void configureBackgroundColor() {
 		this.setStyle(STRING.COLORS.FX_BACKGROUND_COLOR_PREDICATE+STRING.COLORS.DEFAULT_FX_BACKGROUND_COLOR);
 	}
-	
+
 	private void configureSideBarAndButtonSizing(double width) {
-		
+
 		this.sideBarWidth = width * DOUBLE.PERCENT.TEN_PERCENT;
 		this.buttonWidth = sideBarWidth * DOUBLE.PERCENT.NINETY_PERCENT;
-		
+
 	}
-	
+
 	private void configureViewableAreaSizing(double width, double height) {
 
 		this.viewableArea.setPrefSize(width - 2 * this.sideBarWidth, height - DOUBLE.MENU_BAR_HEIGHT);
 
 		// Set margins for nice padding
 		BorderPane.setMargin(viewableArea, new Insets(5, 5, 5, 5));
-		
+
 	}
-	
-	
+
+
 	// All other instance methods
 	protected void sizeMenuImageView(ImageView imageView, double width, double height) {
-		
+
 		imageView.setFitWidth(width);
 		imageView.setFitHeight(height);
-		
+
 	}
 
 	protected Button makeButtonForPane(String text, EventHandler<ActionEvent> lambda) {
