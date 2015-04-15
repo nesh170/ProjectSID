0d25325001c05c328237c75084f51d901572abd6
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 07:25:25 -0400

    implements snapToPixel and sizes in SIDPixel

--- src/levelPlatform/levelPlatformView/SIDPixel.java
+++ src/levelPlatform/levelPlatformView/SIDPixel.java
@@ -11,6 +11,31 @@ import javafx.scene.layout.Region;
  */
 public class SIDPixel extends Region {
 
+	public SIDPixel(double lengthSIDPixel) {
+
+		configureSnapToPixelProperty();
+		configureSIDPixelSize(lengthSIDPixel);
+
+
+
+	}
+
+	private void configureSnapToPixelProperty() {
+
+		// http://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/Region.html#snapToPixelProperty
+		// This defaults to true, which is generally the expected behavior in order to have crisp user interfaces. 
+		// A value of false will allow for fractional alignment, which may lead to "fuzzy" looking borders.
+		this.snapToPixelProperty().set(false);
+
+	}
+
+	private void configureSIDPixelSize(double lengthSIDPixel) {
+
+		this.setPrefWidth(lengthSIDPixel);
+		this.setPrefHeight(lengthSIDPixel);
+
+	}
+
 	
-	
+
 }
