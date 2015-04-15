2ee29c0a13cef02d400fb2d355e796db2c52e588
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 07:41:50 -0400

    works

--- src/levelPlatform/levelPlatformView/LevelPlatformView.java
+++ src/levelPlatform/levelPlatformView/LevelPlatformView.java
@@ -131,10 +131,6 @@ public class LevelPlatformView extends ScrollPane {
 			
 			this.sidPixels.getChildren().add(new SIDPixel(lengthSidePixel));
 			
-			if (TESTING) {
-				System.out.println("Sid pixel: "+i);
-			}
-			
 		}
 		
 		
