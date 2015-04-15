07432babb8ff5716e8b5ac8ee7701898aa467569
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-15 10:16:18 -0400

    done testing (for now) of the LevelPlatformView

--- src/tester/levelPlatform/levelPlatformView/LevelPlatformViewTester.java
+++ src/tester/levelPlatform/levelPlatformView/LevelPlatformViewTester.java
@@ -1,6 +1,10 @@
 package tester.levelPlatform.levelPlatformView;
 
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import levelPlatform.level.EditMode;
+import levelPlatform.level.Level;
+import levelPlatform.levelPlatformView.LevelPlatformView;
 import tester.Tester;
 
 
@@ -18,10 +22,21 @@ public class LevelPlatformViewTester extends Tester {
 	@Override
 	protected void test(Stage stage) {
 
-//		LevelPlatformView levelPlatformView = new LevelPlatformView();
+		BorderPane borderPane = new BorderPane();
+		this.addNode(borderPane);
+		
+		Level level = new Level(100, 50);
+		LevelPlatformView levelPlatformView = new LevelPlatformView(level, EditMode.EDIT_MODE_ON, 10.0);
+		
+		borderPane.setCenter(levelPlatformView);
+		
+		System.out.println(levelPlatformView.getWidth());
+		System.out.println(levelPlatformView.getHeight());
+
+		// At this point verified that the TilePane needs to have a height set on it
+			// --> LevelEditScreen can dynamically find, let's say 50% of the width and 40% of the height, and
+			// allocate that towards the TilePane
 		
 	}
-
-	
 	
 }
