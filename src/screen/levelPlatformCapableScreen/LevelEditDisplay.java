3867a4aff0c4a3960ff6186776e475ad6d71e669
EmperorLeo <laf22@duke.edu>
2015-04-19 04:22:08 -0400

    new class that acts as a model for the level edit screen, it is basically the scroll pane that visually represents the level to the user for the authoring environment ONLY

---
+++ src/screen/levelPlatformCapableScreen/LevelEditDisplay.java
@@ -0,0 +1,102 @@
+package screen.levelPlatformCapableScreen;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import resources.constants.INT;
+import sprite.Sprite;
+import javafx.event.EventHandler;
+import javafx.scene.ImageCursor;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class LevelEditDisplay extends ScrollPane {
+	
+	private Pane content;
+	private int increaseLevelSize;
+	private Map<Sprite,ImageView> spriteToImageMap;
+
+	
+	public LevelEditDisplay(int width, int height, List<Sprite> listOfSprites) {
+		this();
+		content.setMinWidth(width);
+		content.setMinHeight(height);
+		listOfSprites.forEach(sprite -> addSpriteToDisplay(sprite,sprite.spriteImage().getImageViewToDisplay()));
+	}
+	
+	
+	public LevelEditDisplay() {
+		
+		this.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		this.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+				
+		content = new Pane();
+		this.setContent(content);
+		increaseLevelSize = INT.DEFAULT_INCREASE_LEVEL_SIZE;
+		spriteToImageMap = new HashMap<>();
+		
+	}
+	
+	public void setContentMinSize() {
+		
+		content.setMinSize(this.getWidth(), this.getHeight());
+		
+	}
+	
+	public void setSizeToIncrease(int growth) {
+		increaseLevelSize = growth;
+	}
+	
+	public void addWidthLeft() {
+		content.getChildren().forEach(image -> moveRight(image));
+		addWidthRight();
+	}
+	
+	public void addWidthRight() {
+		content.setMinWidth(content.getMinWidth() + increaseLevelSize);
+	}
+	
+	public void addHeightDown() {
+		content.setMinHeight(content.getMinHeight() + increaseLevelSize);
+	}
+	
+	public void addHeightUp() {
+		content.getChildren().forEach(image -> moveDown(image));
+		addHeightDown();
+	}
+	
+	public ImageView getImage(Sprite sprite) {
+		return spriteToImageMap.get(sprite);
+	}
+	
+	public void setListenerToContent(EventHandler<MouseEvent> m) {
+		content.setOnMouseReleased(m);
+	}
+	
+	public void setCursor(Image image) {
+		content.setCursor(new ImageCursor(image));
+	}
+		
+	private void moveRight(Node n) {
+		n.setTranslateX(n.getTranslateX() + increaseLevelSize);
+	}
+	
+	private void moveDown(Node n) {
+		n.setTranslateY(n.getTranslateY() + increaseLevelSize);
+	}
+	
+	public void addSpriteToDisplay(Sprite sprite, ImageView image) {
+		spriteToImageMap.put(sprite, image);
+		image.setTranslateX(sprite.getX());
+		image.setTranslateY(sprite.getY());
+		content.getChildren().add(image);
+	}
+
+
+}
