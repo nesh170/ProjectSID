1de710c5a9002a2e5b0573ebbd4af3df1d995f74
Yongjiao <yongjiao.yu@duke.edu>
2015-04-21 23:00:02 -0400

    removed an unncessary file

--- src/screen/Animation.java
+++ src/screen/Animation.java
@@ -12,6 +12,23 @@ import javafx.scene.control.Button;
 public class Animation {
 	
 	/**
+	 * animates it a node needs to be removed from screen
+	 * @param n
+	 * @param destination
+	 */
+	public static void disposeNode(Node n, Point2D destination){
+		
+		
+	}
+
+	/**
+	 * shakes popup when user did not enter game info or inapproporiate input and ask user to enter again.
+	 * @param args
+	 */
+	public static void shakePopUpWhenError(Node popup){
+		
+	}
+	/**
 	 * This shows an visual effect of a button (ex: jumps slightly backwards) when it is pressed 
 	 * when built in java.button class is not the best option or 
 	 * different visual effect of mouse click is wanted
@@ -20,22 +37,6 @@ public class Animation {
 	public static void buttonPressed(Node button){
 		
 	}
-	/**
-	 * special effects when a node is trashed and needs to be removed out of screen
-	 * @param n
-	 * @param destination
-	 */
-	public static void trashNode(Node n, Point2D destination){
-		
-	}
-	/**
-	 * shakes popup when user did not enter game info or inapproporiate input and ask user to enter again.
-	 * @param args
-	 */
-	public static void shakePopUpWhenError(Node popup){
-		
-	}
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
