9c037c382309a294c27c44e9e0fdefb7eb38f089
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-04-23 19:20:30 -0400

    using parse now

--- src/socCenter/UserPack.java
+++ src/socCenter/UserPack.java
@@ -5,9 +5,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.parse4j.Parse;
+import org.parse4j.ParseException;
+import org.parse4j.ParseObject;
+
 import data.DataHandler;
 
 public class UserPack {
+	
+	private static String APP_ID = "M82RHsJGWvuQQAjCgUms444ujpCWPuJISu5FzZO0";
+	private static String REST_KEY = "M0N668zWJ3n4PE8nPT6FfgYN5RkZ9y9YEs3eahst";
 	
 	private Map<String, User> allUsers;
 	
@@ -15,6 +22,11 @@ public class UserPack {
 		
 		allUsers = new HashMap<String, User>();
 		
+	}
+	
+	public static void setupParse() {
+		System.out.println("setupParse(): initializing...");
+		Parse.initialize(APP_ID, REST_KEY);
 	}
 	
 	public void addUser(User newUser){
@@ -33,5 +45,20 @@ public class UserPack {
 			return null;
 			
 		}
+	}
+	
+	public static void main(String[] args){
+		//Parse.initialize("APP_ID", "REST_KEY");
+		setupParse();
+		ParseObject g = new ParseObject("User");
+		g.put("userName", "Emm");
+		g.put("password", "dona");
+		try {
+			g.save();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
