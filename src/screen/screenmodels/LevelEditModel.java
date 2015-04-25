4d83fa53f9ee303b2f1008e294c5fc790e1eb4e5
EmperorLeo <laf22@duke.edu>
2015-04-25 14:24:55 -0400

    moving stuff

--- src/screen/screenmodels/LevelEditModel.java
+++ src/screen/screenmodels/LevelEditModel.java
@@ -1,6 +1,38 @@
 package screen.screenmodels;
 
+import java.util.Map;
+import java.util.Set;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+import sprite.Sprite;
+import levelPlatform.level.Level;
+
 public class LevelEditModel {
+	
+	private Level level;
+	
+	private Sprite spriteToAdd;
+	private Sprite selectedSprite;
+	
+	private Image imageToAdd;
+
+	private final ObservableList<String> listOfPlatforms = FXCollections.observableArrayList();
+	private final ObservableList<String> listOfEnemies = FXCollections.observableArrayList();
+	private final ObservableList<String> listOfPlayers = FXCollections.observableArrayList();
+	private final ObservableList<String> listOfPowerups = FXCollections.observableArrayList();
+	private final ObservableList<String> listOfOther = FXCollections.observableArrayList();
+	
+	private Map<String,ObservableList<String>> stringToListMap;
+	private Map<String,Sprite> stringToSpriteMap;
+
+	private Set<String> tags;
+	
+	private final static double UNSELECT = 1;
+	private final static double SELECT = 0.4;
+
+
 
 	public LevelEditModel() {
 		// TODO Auto-generated constructor stub
