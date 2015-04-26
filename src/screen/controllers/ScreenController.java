345cd6761953e4167e42dd821cc93d70771cce58
Yongjiao Yu <yongjiao.yu@duke.edu>
2015-04-25 22:08:19 -0400

    removed testing code

--- src/screen/controllers/ScreenController.java
+++ src/screen/controllers/ScreenController.java
@@ -135,7 +135,7 @@ import util.ErrorHandler;
 
 public class ScreenController {
 	//Testing:
-	private boolean GameEdit_Test = true;
+	private boolean GameEdit_Test = false;
 	
 	// Static Variables
 	
